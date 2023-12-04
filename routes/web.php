27dfe113b4d0845d13c6cb1381441a7f45e1d289
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-12-04 17:25:02 +0300

    routes

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\CommentaireController;
+use App\Http\Controllers\ProfileController;
+// use App\Http\Controllers\Publication;
+use App\Http\Controllers\PublicationController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', [PublicationController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/dashboard/commentaire',[CommentaireController::class,'store'])->name('commentaire.store');
+    Route::post('/dashboard/publication',[PublicationController::class,'store'])->name('publication.store');
+});
+
+require __DIR__.'/auth.php';
